1f90b8eea9c7609ad424528e1b6fcba577e33514
Chandrakant <chandrakant@ChandrakantsMBP.lan>
2022-06-02 23:24:39 -0400

    Added Eureka server service to register PMS producer/consumer services on eureka

---
+++ src/test/java/com/cds/org/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cds.org.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
